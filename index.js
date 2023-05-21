15546feda98b4bb838b2b2ff61a108bc882c6a66
Murshed62 <shesir.murshed@gmail.com>
2023-05-21 21:12:46 +0600

    server all done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,10 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
 //noSQL database Mongodb code
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cpvz2.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -22,44 +19,86 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
-    const productCollection = client.db('toyMarketPlace').collection("products")
+    const addedToyCollection = client.db('toyMarketPlace').collection('products')
 
 
-    app.post('/products', async(req,res)=>{
-      const newProduct = req.body;
-      console.log(newProduct);
-      const result = await productCollection.insertOne(newProduct);
-      res.send(result);
-    })
-
-    app.get('/toy', async(req, res)=> {
+    app.get('/products', async(req, res)=>{
+      
       let query = {};
-      const sort = req.query.sort;
-      if(req.query?.email){
-        query = {email:req.query.email}
-      }else if(sort){
-        const result = await productCollection.find(query) .sort({price:sort}).toArray()
-        res.send(result);
-      }else{
-        const result = await productCollection.find(query) .toArray()
-        res.send(result);
+      const sorting =req.query.sorting;
+      if(req.query?.sellerEmail){
+          query= {sellerEmail: req.query.sellerEmail}
       }
+      if(sorting){
+        const result = await addedToyCollection.find(query).sort({price:sorting}).toArray()
+        return res.send(result)
+      }
+      
+      const result = await addedToyCollection.find(query).limit(20).toArray()
+      res.send(result)
+  })
+  app.get("/searchToy/:text", async (req, res) => {
+    const text = req.params.text;
+    const result = await addedToyCollection
+      .find({
+        $or: [
+          { toyName: { $regex: text, $options: "i" } },
+          
+        ],
+      })
+      .toArray();
+    res.send(result);
+  });
+  
+    app.post('/products', async(req,res) =>{
+      const addToy = req.body;
+
+      console.log(addToy)
+      const result = await addedToyCollection.insertOne(addToy)
+      res.send(result)
     })
+    app.get('/products/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id :new ObjectId(id)}
+      const result = await addedToyCollection.findOne(query);
+      res.send(result)
+    })
+    app.put('/products/:id', async(req, res) =>{
+      const id = req.params.id;
+      const filter = {_id : new ObjectId(id)}
+      const options = { upsert: true };
+      const updatedToy = req.body;
+      const updateToy = {
+        $set:{
+          price: updatedToy.price, 
+          rating: updatedToy.rating,
+          quantity: updatedToy.quantity,
+          description: updatedToy.description
 
-    
+        }
+      }
+      const result = await addedToyCollection.updateOne(filter, updateToy, options)
+      res.send(result)
 
-
-
+    })
+    app.delete('/products/:id', async(req, res) =>{
+      const id = req.params.id;
+      console.log(id)
+      const query = { _id : new ObjectId(id)}
+      const result= await addedToyCollection.deleteOne(query);
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
+    await client.db("toyMarketPlace").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -70,11 +109,10 @@ run().catch(console.dir);
 
 //-------------------------------
 
+app.get("/", (req, res) => {
+  res.send("action figure toys");
+});
 
-app.get('/', (req, res) => {
-    res.send("action figure toys");
-})
-
-app.listen(port, () =>{
-    console.log(`action figure toys running on port: ${port}`);
-})
+app.listen(port, () => {
+  console.log(`action figure toys running on port: ${port}`);
+});
