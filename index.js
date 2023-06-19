afc48d17bf8722873d162434e165438f50c1394b
Murshed62 <shesir.murshed@gmail.com>
2023-06-19 18:29:48 +0600

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+// const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
@@ -27,79 +28,87 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const addedToyCollection = client.db('toyMarketPlace').collection('products')
+    const addedToyCollection = client
+      .db("toyMarketPlace")
+      .collection("products");
 
+      // jwt
+      // app.post('/jwt', (req,res) =>{
+      //   const user = req.body;
+      //   console.log(user);
+      //   const token  = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      //     expiresIn: '1h'
+      //   });
+      //   res.send(token);
+      // })
 
-    app.get('/products', async(req, res)=>{
-      
-      let query = {};
-      const sorting =req.query.sorting;
-      if(req.query?.sellerEmail){
-          query= {sellerEmail: req.query.sellerEmail}
+    app.get("/products", async (req, res) => {
+      let sortPrice = req.query?.sort;
+      let query = {}
+      if(sortPrice){
+        query = {price:sortPrice} 
       }
-      if(sorting){
-        const result = await addedToyCollection.find(query).sort({price:sorting}).toArray()
-        return res.send(result)
-      }
+      const result = await addedToyCollection.find().sort(query).toArray();
+      res.send(result);
       
-      const result = await addedToyCollection.find(query).limit(20).toArray()
-      res.send(result)
-  })
-  app.get("/searchToy/:text", async (req, res) => {
-    const text = req.params.text;
-    const result = await addedToyCollection
-      .find({
-        $or: [
-          { toyName: { $regex: text, $options: "i" } },
-          
-        ],
-      })
-      .toArray();
-    res.send(result);
-  });
-  
-    app.post('/products', async(req,res) =>{
+    });
+
+    app.get("/searchToy/:text", async (req, res) => {
+      const text = req.params.text;
+      const result = await addedToyCollection
+        .find({
+          $or: [{ toyName: { $regex: text, $options: "i" } }],
+        })
+        .toArray();
+      res.send(result);
+    });
+
+    app.post("/products", async (req, res) => {
       const addToy = req.body;
 
-      console.log(addToy)
-      const result = await addedToyCollection.insertOne(addToy)
-      res.send(result)
-    })
-    app.get('/products/:id', async(req, res) =>{
+      console.log(addToy);
+      const result = await addedToyCollection.insertOne(addToy);
+      res.send(result);
+    });
+    app.get("/products/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id :new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await addedToyCollection.findOne(query);
-      res.send(result)
-    })
-    app.put('/products/:id', async(req, res) =>{
+      res.send(result);
+    });
+    app.put("/products/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id : new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
       const updatedToy = req.body;
       const updateToy = {
-        $set:{
-          price: updatedToy.price, 
+        $set: {
+          price: updatedToy.price,
           rating: updatedToy.rating,
           quantity: updatedToy.quantity,
-          description: updatedToy.description
-
-        }
-      }
-      const result = await addedToyCollection.updateOne(filter, updateToy, options)
-      res.send(result)
-
-    })
-    app.delete('/products/:id', async(req, res) =>{
+          description: updatedToy.description,
+        },
+      };
+      const result = await addedToyCollection.updateOne(
+        filter,
+        updateToy,
+        options
+      );
+      res.send(result);
+    });
+    app.delete("/products/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id)
-      const query = { _id : new ObjectId(id)}
-      const result= await addedToyCollection.deleteOne(query);
-      res.send(result)
-    })
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await addedToyCollection.deleteOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     await client.db("toyMarketPlace").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
